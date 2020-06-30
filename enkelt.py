77b9b68f39def3ec37e63c2d34b39293a1ac327d
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-06-30 00:33:09 -0400

    Fixes incorrect handling of utf-8 characters (#132)
    * Fixes incorrect handling of utf-8 characters
    
    Changes `with open(..., 'r+') as f:` to `with codecs.open(..., 'r', 'utf-8') as f:` and `with open(..., 'w+') as f:` to `with codecs.open(..., 'w', 'utf-8') as f:`.
    
    * Changes Codecs to IO

--- enkelt.py
+++ enkelt.py
@@ -862,7 +862,7 @@ def run_transpiled_code():
 
     if is_console_mode is False:
         # Writes the transpiled code to a file temporarily.
-        with open('final_transpiled.py', 'w+')as transpiled_f:
+        with open('final_transpiled.py', 'w', encoding='utf-8') as transpiled_f:
             transpiled_f.writelines(code)
 
     # Executes the code transpiled to python and catches Exceptions
@@ -889,7 +889,7 @@ def run_transpiled_code():
 
     if is_console_mode is False:
         # Removes the temporary python file.
-        with open('final_transpiled.py', 'w+')as transpiled_f:
+        with open('final_transpiled.py', 'w', encoding='utf-8') as transpiled_f:
             transpiled_f.writelines('')
         os.remove(os.getcwd() + '/final_transpiled.py')
 
@@ -1031,7 +1031,7 @@ if not is_dev:
                 if sys.argv[2] == '--d':
                     is_developer_mode = True
 
-            with open(enkelt_script_path, 'r+')as f:
+            with open(enkelt_script_path, 'r', encoding='utf-8') as f:
                 tmp_code_to_run = f.readlines()
 
             prepare_and_run_code_lines_to_be_run(tmp_code_to_run)
